c70ad1114895cb38c269be686356dd3a41f59368
weiyuan <weiyuan@100tal.com>
2019-03-20 16:44:02 +0800

    add flow-test and flow.md

--- testvue/flow-test/index.js
+++ testvue/flow-test/index.js
@@ -3,29 +3,29 @@
  * @LastEditors: Wy
  * @Description: 
  * @Date: 2019-03-20 15:42:10
- * @LastEditTime: 2019-03-20 16:32:13
+ * @LastEditTime: 2019-03-20 16:43:41
  */
 /*
 /* @flow */
 
-function split(str) {
-  return str.split(' ')
-}
+// function split(str) {
+//   return str.split(' ')
+// }
 
-split(11)
+// split(11)
 
 
-function add(x: number, y: number): number {
-  return x + y
-}
+// function add(x: number, y: number): number {
+//   return x + y
+// }
 
-add('Hello', 11)
+// add('Hello', 11)
 
-function add(x: number, y: number): number {
-  return x + y
-}
+// function add(x: number, y: number): number {
+//   return x + y
+// }
 
-add(12, 11)
+// add(12, 11)
 
 
 // 数组类型注释的格式是 Array<T>   T表示数组中每项的数据类型。
